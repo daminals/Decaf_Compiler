2ebdfd1699cdaba04191a45eba4eafa99fd473ae
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2023-02-22 15:17:06 -0500

    add reserved and error text

--- decaf_lexer.py
+++ decaf_lexer.py
@@ -4,9 +4,38 @@
 # 02.21.2023
 
 import ply.lex as lex
+TERMINAL_RED_PRINT = '\033[91m'
+TERMINAL_CLEAR_PRINT = '\033[0m'
+
+# List of reserved keywords for functions
+reserved = {
+   'boolean' : 'BOOLEAN',
+   'break' : 'BREAK',
+   'extends' : 'EXTENDS',
+   'false' : 'FALSE',
+   'new' : 'NEW',
+   'null' : 'NULL',
+   'super' : 'SUPER',
+   'this' : 'THIS',
+   'continue' : 'CONTINUE',
+   'class' : 'CLASS',
+   'float' : 'FLOAT',
+   'for' : 'FOR',
+   'private' : 'PRIVATE',
+   'public' : 'PUBLIC',
+   'true' : 'TRUE',
+   'void' : 'VOID',
+   'while' : 'WHILE',
+   'do' : 'DO',
+   'else' : 'ELSE',
+   'if' : 'IF',
+   'int' : 'INT',
+   'return' : 'RETURN',
+   'static' : 'STATIC'
+}
 
 # List of token names.   This is always required
-tokens = (
+tokens = [
    'NUMBER',
    'PLUS',
    'MINUS',
@@ -15,6 +44,8 @@ tokens = (
    'LPAREN',
    'RPAREN',
     'LCURLY',
+    'RBRACKET',
+    'LBRACKET',
    'RCURLY',
     'EQUAL',
     'NOTEQUAL',
@@ -25,8 +56,13 @@ tokens = (
     'OR',
     'AND',
     'NOT',
-
-)
+    'TRUE',
+    'FALSE',
+    'SETEQUAL',
+    'SEMICOLON',
+    'ERROR', # error type, no rule associated with type except throw err
+    'ID'
+] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
@@ -37,6 +73,8 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_LCURLY  = r'\{'
 t_RCURLY  = r'\}'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 t_EQUAL = r'\=\='
 t_NOTEQUAL = r'\!\='
 t_GREATER = r'\>'
@@ -46,6 +84,8 @@ t_LESSEQ = r'\<\='
 t_OR = r'\|\|'
 t_AND = r'\&\&'
 t_NOT = r'\!'
+t_SETEQUAL = r'\='
+t_SEMICOLON = r'\;'
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
@@ -53,13 +93,51 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+def t_TRUE(t):
+    r'true'
+    t.value = True
+    return t
+
+def t_FALSE(t):
+    r'false'
+    t.value = False
+    return t
+
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+# Compute column.
+# input is the input text string
+# token is a token instance
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
+# EOF handling rule
+def t_eof(t):
+    # Get more input (Example)
+    more = input('... ')
+    if more:
+        self.lexer.input(more)
+        return self.lexer.token()
+    return None
+
+
 # A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t'
+
+# rules for reserved words
+# needs to be last in the file since regex is greedy
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    if t.value in reserved:             # Check for reserved words
+      t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    elif t.value not in tokens:
+        print(f"{TERMINAL_RED_PRINT}Invalid token: {t.value}{TERMINAL_CLEAR_PRINT}")
+        t.type = 'ERROR'
+    return t
 
 # Error handling rule
 def t_error(t):
@@ -71,6 +149,10 @@ lexer = lex.lex()
 data = ''' 3
  + 4 * 10
   + -20 *2
+invalid
+  IFFYf (true) {
+    5+2
+  };
 '''
 
 lexer.input(data)
