3b687e58ddbba2ab9f135cb6b0fb062574b64779
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2023-02-26 09:19:39 -0500

    lex

--- src/decaf_lexer.py
+++ src/decaf_lexer.py
@@ -9,44 +9,44 @@ TERMINAL_CLEAR_PRINT = '\033[0m'
 
 # List of reserved keywords for functions
 reserved = {
-   'boolean' : 'BOOLEAN',
-   'break' : 'BREAK',
-   'extends' : 'EXTENDS',
-   'false' : 'FALSE',
-   'new' : 'NEW',
-   'null' : 'NULL',
-   'super' : 'SUPER',
-   'this' : 'THIS',
-   'continue' : 'CONTINUE',
-   'class' : 'CLASS',
-   'float' : 'FLOAT',
-   'for' : 'FOR',
-   'private' : 'PRIVATE',
-   'public' : 'PUBLIC',
-   'true' : 'TRUE',
-   'void' : 'VOID',
-   'while' : 'WHILE',
-   'do' : 'DO',
-   'else' : 'ELSE',
-   'if' : 'IF',
-   'int' : 'INT',
-   'return' : 'RETURN',
-   'static' : 'STATIC'
+    'boolean': 'BOOLEAN',
+    'break': 'BREAK',
+    'extends': 'EXTENDS',
+    'false': 'FALSE',
+    'new': 'NEW',
+    'null': 'NULL',
+    'super': 'SUPER',
+    'this': 'THIS',
+    'continue': 'CONTINUE',
+    'class': 'CLASS',
+    'float': 'FLOAT',
+    'for': 'FOR',
+    'private': 'PRIVATE',
+    'public': 'PUBLIC',
+    'true': 'TRUE',
+    'void': 'VOID',
+    'while': 'WHILE',
+    'do': 'DO',
+    'else': 'ELSE',
+    'if': 'IF',
+    'int': 'INT',
+    'return': 'RETURN',
+    'static': 'STATIC'
 }
 
 # List of token names.   This is always required
 tokens = [
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
     'LCURLY',
     'RBRACKET',
     'LBRACKET',
-   'RCURLY',
+    'RCURLY',
     'EQUAL',
     'NOTEQUAL',
     'GREATER',
@@ -59,19 +59,19 @@ tokens = [
     'BOOL',
     'SETEQUAL',
     'SEMICOLON',
-    'ERROR', # error type, no rule associated with type except throw err
+    'ERROR',  # error type, no rule associated with type except throw err
     'ID'
 ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LCURLY  = r'\{'
-t_RCURLY  = r'\}'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
 t_EQUAL = r'\=\='
@@ -113,6 +113,7 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+
 def t_BOOL(t):
     r'(true|false)'
     t.value = True if t.value == 'true' else False
@@ -139,6 +140,8 @@ def t_BOOL(t):
 #     return t
 
 # Define a rule so we can track line numbers
+
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -146,9 +149,12 @@ def t_newline(t):
 # Compute column.
 # input is the input text string
 # token is a token instance
+
+
 def find_column(input, token):
     line_start = input.rfind('\n', 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
+
 
 # EOF handling rule
 '''def t_eof(t):
@@ -161,23 +167,28 @@ def find_column(input, token):
 
 
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore = ' \t'
 
 # rules for reserved words
 # needs to be last in the file since regex is greedy
+
+
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     if t.value in reserved:             # Check for reserved words
-      t.type = reserved.get(t.value,'ID')    # Check for reserved words
+        t.type = reserved.get(t.value, 'ID')    # Check for reserved words
     elif t.value not in tokens:
         print(f"{TERMINAL_RED_PRINT}Invalid token: {t.value}{TERMINAL_CLEAR_PRINT}")
         t.type = 'ERROR'
     return t
 
 # Error handling rule
+
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 # Build the lexer
 lexer = lex.lex()
