4a8e20d2c3dca197a26e79fc8cd3763ff4e55fbe
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2023-02-28 14:14:53 -0500

    increase unit test clarity

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class TestDecafFiles(unittest.TestCase):
       cmd_str = f"python src/decaf_checker.py {file}"
       process = subprocess.Popen(cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       stdout, stderr = process.communicate()
-      err_message = f"{RED}{file}{CLEAR_FORMAT} {UNDERLINE}exited with return code {process.returncode}, not {expected_return_code}{CLEAR_FORMAT}"
+      err_message = f"{RED}{file}{CLEAR_FORMAT} {UNDERLINE}exited with return code {process.returncode}, not {expected_return_code}{CLEAR_FORMAT}\n {GREEN}Output:{CLEAR_FORMAT} {stdout.decode()}\n\n {RED}Error:{CLEAR_FORMAT} {stderr.decode()}"
       # print(f"{GREEN}{file}{CLEAR_FORMAT}: {stdout}")
       testcase_class.assertEqual(expected_return_code, process.returncode, err_message)
       return stdout, stderr
